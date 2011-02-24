d2fd38d56d2c7a400d9c6047fa0ab43386290989
Kevin Thompson <mrunleaded@gmail.com>
2011-02-23 22:43:57 -0800

    Added deflate tool to help fix ObjectWriterTests.
    Signed-off-by: Kevin Thompson <mrunleaded@gmail.com>

---
+++ Tools/deflate/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+
+namespace decompress
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+                return;
+
+            var file = args[0];
+
+            var outfile = file + ".bin";
+            using (var cfile = new CompressionStream(file))
+            {
+                using (var ufile = File.OpenWrite(outfile))
+                {
+                    cfile.CopyTo(ufile);
+                }
+            }
+        }
+    }
+
+    internal class CompressionStream : DeflateStream
+    {
+        private static Stream MoveStream(Stream stream)
+        {
+            // HACK: we need this to get the DeflateStream to read properly
+            stream.ReadByte();
+            stream.ReadByte();
+            return stream;
+        }
+
+        private static FileStream OpenFile(string fileLocation)
+        {
+            return new FileStream(fileLocation, FileMode.Open, FileAccess.Read, FileShare.Read);
+        }
+
+        public CompressionStream(string fileLocation, CompressionMode mode = CompressionMode.Decompress, bool leaveOpen = false)
+            : this(OpenFile(fileLocation), mode, leaveOpen)
+        {
+
+        }
+
+        public CompressionStream(Stream stream, CompressionMode mode = CompressionMode.Decompress, bool leaveOpen = false)
+            : base(MoveStream(stream), mode, leaveOpen)
+        {
+        }
+    }
+}
